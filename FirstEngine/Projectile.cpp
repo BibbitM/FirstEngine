0f9151d3a923c382bb8e4228ff7c10088af4005b
Michał Nowak <bibbit@wp.pl>
2018-11-29 14:17:22 +0100

    Added projectile class

---
+++ FirstEngine/Projectile.cpp
@@ -0,0 +1,125 @@
+#include "Projectile.h"
+#include "CollisionResult.h"
+#include "Collisions.h"
+#include "Level.h"
+#include "Math.h"
+#include "ShapeAabb.h"
+#include "ShapePlane.h"
+#include "ShapeSphere.h"
+#include "Terrain.h"
+#include <algorithm>
+
+#ifdef max
+#undef max
+#endif
+
+float Projectile::s_gravity = 10.0f;
+
+float Projectile::s_minimalScale = 0.01f;
+
+
+Projectile::Projectile()
+	: m_radius( 0.0f )
+	, m_lifeTime( 0.0f )
+	, m_gravityFactor( 0.0f )
+	, m_lifeTimeConsumed( 0.0f )
+{
+	SetMesh( "Content\\unitsphere.x" );
+	SetColor( 0xFFFF0000 );
+
+	float scale = std::max( m_radius, s_minimalScale );
+	SetActorScale( D3DXVECTOR3( scale, scale, scale ) );
+}
+
+
+Projectile::~Projectile() = default;
+
+void Projectile::SetRadius( float radius )
+{
+	m_radius = radius;
+
+	float scale = std::max( m_radius, s_minimalScale );
+	SetActorScale( D3DXVECTOR3( scale, scale, scale ) );
+}
+
+ShapeSphere Projectile::GetCollisionSphere() const
+{
+	ShapeSphere sphere;
+
+	sphere.m_center = GetActorWorldPosition();
+	sphere.m_radius = m_radius;
+
+	return sphere;
+}
+
+void Projectile::OnUpdate( float deltaTime )
+{
+	UpdateVelocity( deltaTime );
+
+	// Base class simulates movement.
+	Character::OnUpdate( deltaTime );
+
+	UpdateCollision();
+
+	// Call it as last. It can delete this.
+	UpdateLifetTime( deltaTime );
+}
+
+void Projectile::UpdateVelocity( float deltaTime )
+{
+	if( m_gravityFactor == 0.0f )
+	{
+		return;
+	}
+
+	SetVelocity( GetVelocity() - Math::s_upVector3 * s_gravity * m_gravityFactor * deltaTime );
+}
+
+void Projectile::UpdateCollision()
+{
+	CollisionResult collision;
+
+	if( !GetNearestCollision( collision ) )
+	{
+		return;
+	}
+
+
+	float collisionDot = D3DXVec3Dot( &collision.m_normal, &GetVelocity() );
+
+	// Check if can bounce.
+	if( collisionDot >= 0.0f )
+	{
+		return;
+	}
+
+	// Bounce.
+	SetVelocity( GetVelocity() - collision.m_normal * collisionDot * 2.0f );
+}
+
+void Projectile::UpdateLifetTime( float deltaTime )
+{
+	if( m_lifeTime <= 0.0f )
+	{
+		return;
+	}
+
+	m_lifeTimeConsumed += deltaTime;
+
+	if( m_lifeTimeConsumed >= m_lifeTime )
+	{
+		Destroy();
+	}
+}
+
+bool Projectile::GetNearestCollision( CollisionResult& result ) const
+{
+	ShapeSphere mySphere = GetCollisionSphere();
+
+	// Collision with ground.
+	ShapePlane ground;
+	ground.m_point = D3DXVECTOR3( 0.0f, 0.0f, 0.0f );
+	ground.m_normal = Math::s_upVector3;
+
+	return Collisions::CheckCollisonSpherePlane( result, mySphere, ground );
+}
---
+++ FirstEngine/Projectile.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include "Character.h"
+
+class CollisionResult;
+class ShapeSphere;
+
+class Projectile : public Character
+{
+public:
+	Projectile();
+	~Projectile();
+
+	float GetRadius() { return m_radius; }
+	void SetRadius( float radius );
+
+	float GetLifeTime() const { return m_lifeTime; }
+	void SetLifeTime( float time ) { m_lifeTime = time; }
+
+	float GetGravityFactor() const { return m_gravityFactor; }
+	void SetGravityFactor( float factor ) { m_gravityFactor = factor; }
+
+	ShapeSphere GetCollisionSphere() const;
+
+protected:
+	void OnUpdate( float deltaTime );
+
+	void UpdateVelocity( float deltaTime );
+	void UpdateCollision();
+	void UpdateLifetTime( float deltaTime );
+
+	bool GetNearestCollision( CollisionResult& result ) const;
+
+private:
+	float m_radius;
+	float m_lifeTime;
+	float m_gravityFactor;
+
+	float m_lifeTimeConsumed;
+
+	static float s_gravity;
+	static float s_minimalScale;
+};
+
