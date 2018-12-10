ed74f6639badd757436a46bf6cfa7a0fc80ecea6
Michał Nowak <bibbit@wp.pl>
2018-12-10 09:03:00 +0100

    Added checking collision with test shapes for camera point.

--- FirstEngine/TestCameraPoint.cpp
+++ FirstEngine/TestCameraPoint.cpp
@@ -6,7 +6,10 @@
 #include "InputManager.h"
 #include "Math.h"
 #include "Level.h"
+#include "ShapeAabb.h"
 #include "ShapePlane.h"
+#include "ShapeSphere.h"
+#include "TestCollisionShape.h"
 
 TestCameraPoint::TestCameraPoint()
 {
@@ -28,17 +31,13 @@ void TestCameraPoint::OnUpdate( float deltaTime )
 	D3DXVECTOR3 cameraDir = camera->ScreenToDirection( mousePosX, mousePosY );
 	D3DXVECTOR3 cameraPos = camera->GetPosition();
 
-	ShapePlane ground;
-	ground.m_point = D3DXVECTOR3( 0.0f, 0.0f, 0.0f );
-	ground.m_normal = Math::s_upVector3;
+	CollisionResult collision;
 
-	CollisionResult collResult;
+	D3DXVECTOR3 collisionPos( 0.0f, 0.0f, 0.f );
 
-	D3DXVECTOR3 groundPos( 0.0f, 0.0f, 0.f );
-
-	if( Collisions::LineTracePlane( collResult, cameraPos, cameraDir * 1000.0f, ground ) )
+	if( GetNearestCollision( collision, cameraPos, cameraPos + cameraDir * 1000.0f ) )
 	{
-		groundPos = collResult.m_position;
+		collisionPos = collision.m_position;
 
 		SetColor( 0xFF11CC11 );
 	}
@@ -47,8 +46,71 @@ void TestCameraPoint::OnUpdate( float deltaTime )
 		SetColor( 0xFFCC1111 );
 	}
 
-	SetActorPosition( groundPos );
+	SetActorPosition( collisionPos );
 
 
 	Pawn::OnUpdate( deltaTime );
+}
+
+bool TestCameraPoint::GetNearestCollision( CollisionResult& collision, const D3DXVECTOR3& start, const D3DXVECTOR3& end ) const
+{
+	D3DXVECTOR3 testEnd = end;
+	bool result = false;
+
+	if( GetCollisionWithGround( collision, start, testEnd ) )
+	{
+		result |= true;
+		testEnd = collision.m_position;
+	}
+
+	if( GetNearestCollisionWithTestShapes( collision, start, testEnd ) )
+	{
+		result |= true;
+		testEnd = collision.m_position;
+	}
+
+	return result;
+}
+
+bool TestCameraPoint::GetCollisionWithGround( CollisionResult& collision, const D3DXVECTOR3& start, const D3DXVECTOR3& end ) const
+{
+	ShapePlane ground;
+	ground.m_point = D3DXVECTOR3( 0.0f, 0.0f, 0.0f );
+	ground.m_normal = Math::s_upVector3;
+
+	return Collisions::LineTracePlane( collision, start, end, ground );
+}
+
+bool TestCameraPoint::GetNearestCollisionWithTestShapes( CollisionResult& collision, const D3DXVECTOR3& start, const D3DXVECTOR3& end ) const
+{
+	D3DXVECTOR3 testEnd = end;
+	bool result = false;
+
+	std::vector< TestCollisionShape* > collisionShapes = GetLevel()->GetObjectsFromClass< TestCollisionShape >();
+
+	for( auto shape : collisionShapes )
+	{
+		if( GetCollisionWithTestShape( collision, start, testEnd, *shape ) )
+		{
+			result |= true;
+			testEnd = collision.m_position;
+		}
+	}
+
+	return result;
+}
+
+bool TestCameraPoint::GetCollisionWithTestShape( CollisionResult& collision, const D3DXVECTOR3& start, const D3DXVECTOR3& end, const TestCollisionShape& shape ) const
+{
+	switch( shape.GetShape() )
+	{
+	case Collisions::EShape::Plane:
+		return Collisions::LineTracePlane( collision, start, end, shape.GetCollisionPlane() );
+	case Collisions::EShape::Sphere:
+		return Collisions::LineTraceSphere( collision, start, end, shape.GetCollisionSphere() );
+	case Collisions::EShape::Aabb:
+		return Collisions::LineTraceAabb( collision, start, end, shape.GetCollisionAabb() );
+	default:
+		return false;
+	}
 }
--- FirstEngine/TestCameraPoint.h
+++ FirstEngine/TestCameraPoint.h
@@ -2,6 +2,9 @@
 
 #include "Pawn.h"
 
+class CollisionResult;
+class TestCollisionShape;
+
 class TestCameraPoint : public Pawn
 {
 public:
@@ -9,4 +12,11 @@ public:
 	~TestCameraPoint();
 
 	virtual void OnUpdate( float deltaTime ) override;
+
+private:
+	bool GetNearestCollision( CollisionResult& collision, const D3DXVECTOR3& start, const D3DXVECTOR3& end ) const;
+
+	bool GetCollisionWithGround( CollisionResult& collision, const D3DXVECTOR3& start, const D3DXVECTOR3& end ) const;
+	bool GetNearestCollisionWithTestShapes( CollisionResult& collision, const D3DXVECTOR3& start, const D3DXVECTOR3& end ) const;
+	bool GetCollisionWithTestShape( CollisionResult& collision, const D3DXVECTOR3& start, const D3DXVECTOR3& end, const TestCollisionShape& shape ) const;
 };
