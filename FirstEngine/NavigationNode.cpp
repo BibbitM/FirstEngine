5ad750869af58dfcccb6b9fcd109ae4723931b1e
Michał Nowak <bibbit@wp.pl>
2019-01-02 21:52:19 +0100

    Added NavigatioNode class

---
+++ FirstEngine/NavigationNode.cpp
@@ -0,0 +1,18 @@
+#include "NavigationNode.h"
+#include <cassert>
+
+NavigationNode::NavigationNode()
+	: m_position( 0.0f, 0.0f, 0.0f )
+{
+}
+
+void NavigationNode::AddNeigbour( const NavigationNode* neightbour )
+{
+	assert( neightbour );
+}
+
+float CalculateDistance( const NavigationNode& first, const NavigationNode& second )
+{
+	D3DXVECTOR3 offset = second.GetPosition() - first.GetPosition();
+	return D3DXVec3Length( &offset );
+}
---
+++ FirstEngine/NavigationNode.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <d3dx9math.h>
+#include <vector>
+
+class NavigationNode
+{
+public:
+	NavigationNode();
+
+	void SetPosition( const D3DXVECTOR3& position ) { m_position = position; }
+	const D3DXVECTOR3& GetPosition() const { return m_position; }
+
+	void AddNeigbour( const NavigationNode* neightbour );
+	const std::vector< const NavigationNode* > GetNeightbours() const { return m_neighbours; }
+
+private:
+	D3DXVECTOR3 m_position;
+	std::vector< const NavigationNode* > m_neighbours;
+};
+
+float CalculateDistance( const NavigationNode& first, const NavigationNode& second );
